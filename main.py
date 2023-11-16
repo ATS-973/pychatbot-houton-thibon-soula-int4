4b07322cfd37b2a89e769f86fb3bce25786d6e9c
ATS_973 <82520816+ATS-973@users.noreply.github.com>
2023-11-16 13:40:26 +0100

    On branch Achille Changes to be committed: modified:   main.py

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ def list_of_files(directory, extension):
     return files_names
 
 for i in list_of_files("./speeches","txt"):
-    if i.endswith("0","1","2","3","4","5","6","7","8","9") and i[:-1] not in pres_names:
-        pres_names.append(i)
-    elif i not in pres_names:
-        pres_names.append(i)
+    if i.endswith("0.txt") or i.endswith("1.txt") or i.endswith("2.txt") or i.endswith("3.txt") or i.endswith("4.txt") or i.endswith("5.txt") or i.endswith("6.txt") or i.endswith("7.txt") or i.endswith("8.txt") or i.endswith("9.txt"):
+        if i[:-5].split("_")[-1] not in pres_names:
+            pres_names.append(i[:-5].split("_")[-1])
+            #print(i[:-5].split("_")[-1])
+    elif i[:-4].split("_")[-1] not in pres_names:
+        pres_names.append(i[:-4].split("_")[-1])
+print(pres_names)
