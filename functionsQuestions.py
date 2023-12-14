e9a1fb220cf4a8b8ae64ca3677394bac94baeaee
ATS_973 <82520816+ATS-973@users.noreply.github.com>
2023-12-14 10:33:22 +0100

    Add questions Functions

--- functionsQuestions.py
+++ functionsQuestions.py
@@ -1,3 +1,6 @@
+import tfIdf
+import os
+
 def tokenization(question):
     questionCleared = ""
     for i in question:
@@ -14,4 +17,34 @@ def tokenization(question):
                 questionCleared += " "
     questionCleared = questionCleared.split(" ")
     return(questionCleared)
+
+def wordsInCorpus(corpus, question):
+    questionCleared = tokenization(question)
+    idfList = tfIdf.idf(corpus).keys()
+    finalWords = []
+    for i in questionCleared:
+        if i in idfList:
+            finalWords.append(i)
+    return(finalWords)
+
+def tfQuestion(question):
+    wordsOcc = {}
+    for i in tokenization(question):
+        if i not in wordsOcc:
+            wordsOcc[i] = 1
+        else:
+            wordsOcc[i] += 1
+    for key,value in wordsOcc.items():
+        wordsOcc[key] = 1/value
+        
+    return(wordsOcc)
+
+def matriceQuestion(question, corpus):
+    matQuestion = []
+    mat = tfIdf.matrice(corpus)
+    idfCorpus = tfIdf.idf(corpus)
+    for i in tokenization(question):
+        if i in idfCorpus.keys():
+            matQuestion.append(mat[list(idfCorpus.keys()).index(i)])
+    return(matQuestion)
 
